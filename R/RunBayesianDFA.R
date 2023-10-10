ff811751bf779c1605363d958cb93896b9793fec
Robert <rwilderm@ucsc.edu>
2023-10-10 13:53:31 -0700

    Add test of more than 40 variables to bayesdfa

--- R/RunBayesianDFA.R
+++ R/RunBayesianDFA.R
@@ -4,35 +4,36 @@
 library(tidyverse)
 library(bayesdfa)
 
-datDFA <- read_csv("C:/Users/r.wildermuth/Documents/FutureSeas/RecruitmentIndex/recrmntDFA/recrDFAdat.csv")
+datDFA <- read_csv("recrDFAdat.csv")
 
 allDat <- datDFA %>% filter(year %in% 1980:2019) %>%
             # remove contemporary adult biomass with recruits, should be S2 biomass -> S1 recs
             select(-c(NOI,
                       ENSO,
                       NPGO,
-                      #All_Copepods, # ~same as calanoid copepods
-                      #euphausiids, # too large for larval mouth gape
                       anchBioSmrySeas1,
                       sardBioSmrySeas1,
-                      # anchBioSmrySeas2, # leave out biomass since not fit well
-                      # sardBioSmrySeas2,
+                      anchBioSmrySeas2, # accounted for in S-R relationship
+                      sardBioSmrySeas2,
                       # NCOPspring,
                       # SCOPspring, # summer copepod index had higher loadings
-                      PDOsummer, # lower loading than spring, may want to try a lag
+                      NCOPsummer, # only have NEMURO pojections of spring
+                      SCOPsummer,
+                      PDOsummer, 
                       PDOspring,
-                      # BEUTI_33N, # oceanography at 39N had highest loadings
-                      # OC_LUSI_33N,
-                      # OC_LUSI_36N,
-                      # OC_STI_33N,
-                      # OC_STI_36N,
+                      BEUTI_39N, # keep more local variables
+                      CUTI_39N,
+                      OC_LUSI_39N,
+                      OC_STI_39N,
+                      # avgNearTransspring, # spring transport had lower MARSS loadings       
+                      # avgOffTransspring,
+                      # naupBio, # had low MARSS loadings
                       copMeanSize,
                       copPropBioSize,
                       naupMeanSize,
                       naupPropBioSize,
                       ZL_NorCal,
-                      ZL_SoCal,
-                      # age1SprAnchmeanWAA # not enough data in time window
+                      ZL_SoCal
                       )) 
 
 datNames <- names(allDat)[-1]
@@ -50,19 +51,22 @@ datDense %>% group_by(year) %>%
   ggplot(aes(x = year, y = nMissing)) +
   geom_col()
 
+# overall proportion of missing data
+sum(is.na(allDat))/(dim(allDat)[1]*dim(allDat)[2])
+
 # transpose for MARSS formatting
 allDat <- allDat %>% select(-year) %>% t()
 
-chains <- 3
-iter <- 10
+chains <- 4
+iter <- 5000
 
 # Create a custom R obs error matrix assuming each data source has it's own common error
 Rcustom <- c(1, #"HCI",
-             rep(2, 4), #"COP", "COP", "COP", "COP",
-             3, 3, #"BEUTI", "BEUTI", 
-             4, 4, #"CUTI", "CUTI",
-             5, 5, 5, #"LUSI", "LUSI", "LUSI", 
-             6, 6, 6, #"STI", "STI", "STI",
+             2, 2, #rep(2, 4), #"COP", "COP", "COP", "COP",
+             3, #3, #"BEUTI", "BEUTI", 
+             4, #4, #"CUTI", "CUTI",
+             5, 5, #5, #"LUSI", "LUSI", "LUSI", 
+             6, 6, #6, #"STI", "STI", "STI",
              7, #"RREAS",
              8, 8, #"SSWI", "SSWI",
              9, 9, 9, #"CalCOFI", "CalCOFI", "CalCOFI",
@@ -75,30 +79,38 @@ Rcustom <- c(1, #"HCI",
              13, 13, #"SDM", "SDM",
              15, #"anchRec", 
              16, #"sardRec",
-             17, #"anchBio",
-             18, #"sardBio",
-             19, 19, #"SST", "SST",
-             rep(20, 4), #"Transp", "Transp", "Transp", "Transp",
-             21, #"Alb",
-             22) #"Hake"
+             #17, #"anchBio",
+             #18, #"sardBio",
+             17, 17, #19, 19, #"SST", "SST",
+             rep(18, 4), #"Transp", "Transp", "Transp", "Transp",
+             19, #"Alb",
+             20) #"Hake"
 
-fit1trend <- fit_dfa(
+fit4trends <- fit_dfa(
                 y = allDat, num_trends = 1, scale="zscore",
                 varIndx = rep(1, nrow(allDat)), #rep_len(1:2, nrow(allDat)),
-                # varIndx = 1:47, # mapping of unique R matrix variances to indicators
+                # varIndx = 1:nrow(allDat), # mapping of unique R matrix variances to indicators
                 # varIndx = Rcustom,
-                iter = iter, chains = chains, thin = 1)
+                estimate_trend_ar = TRUE,
+                iter = iter, chains = chains, thin = 1,
+                cores = parallel::detectCores()-2)
+# RW: will not fit models with more than ~40 index time series
+
 
 # check convergence 
-is_converged(fit1trend)
+is_converged(fit4trends)
+
+# need convergence diagnostics
+# thinning?
+shinystan::launch_shinystan(fit4trends$model)
 
 # Rotate trends
-trendsRot <- rotate_trends(fit1trend)
+trendsRot <- rotate_trends(fit4trends)
 
 plot_trends(trendsRot)
 
 # plot the fitted values
-plot_fitted(fit1trend)
+plot_fitted(fit4trends)
 
 # plot the loadings
 plot_loadings(trendsRot) + 
---
+++ R/TestManyVariables.R
@@ -0,0 +1,71 @@
+# Test ability to fit many data variables
+
+library(bayesdfa)
+chains <- 3
+iter <- 10
+
+simDat20 <- sim_dfa(
+              num_trends = 2,
+              num_years = 15,
+              num_ts = 20
+            )
+dim(simDat20$y_sim)
+
+fit1trend <- fit_dfa(
+  y = simDat20$y_sim, num_trends = 1, scale="zscore",
+  varIndx = 1:nrow(simDat20$y_sim), # mapping of unique R matrix variances to indicators
+  iter = iter, chains = chains, thin = 1)
+
+simDat35 <- sim_dfa(
+  num_trends = 2,
+  num_years = 15,
+  num_ts = 35
+)
+dim(simDat35$y_sim)
+
+fit1trend <- fit_dfa(
+  y = simDat35$y_sim, num_trends = 1, scale="zscore",
+  varIndx = 1:nrow(simDat35$y_sim), # mapping of unique R matrix variances to indicators
+  iter = iter, chains = chains, thin = 1)
+
+simDat40 <- sim_dfa(
+  num_trends = 2,
+  num_years = 10000, # initialization eventually worked with this sample size (num_years = 10000)
+  num_ts = 40
+)
+dim(simDat40$y_sim)
+
+fit1trend <- fit_dfa(
+  y = simDat40$y_sim, num_trends = 1, scale="zscore",
+  varIndx = 1:nrow(simDat40$y_sim), # mapping of unique R matrix variances to indicators
+  iter = iter, chains = chains, thin = 1)
+
+simDat41 <- sim_dfa(
+  num_trends = 2,
+  num_years = 100,
+  num_ts = 41
+)
+dim(simDat41$y_sim)
+
+fit1trend <- fit_dfa(
+  y = simDat41$y_sim, num_trends = 1, scale="zscore",
+  varIndx = 1:nrow(simDat41$y_sim), # mapping of unique R matrix variances to indicators
+  iter = iter, chains = chains, thin = 1)
+
+simDat50 <- sim_dfa(
+  num_trends = 2,
+  num_years = 1000,
+  num_ts = 50
+)
+dim(simDat50$y_sim)
+
+fit1trend <- fit_dfa(
+  y = simDat50$y_sim, num_trends = 1, scale="zscore",
+  varIndx = 1:nrow(simDat50$y_sim), # mapping of unique R matrix variances to indicators
+  iter = iter, chains = chains, thin = 1)
+
+bs1trend <- fit_dfa(
+  y = simDat50$y_sim, num_trends = 1, scale="zscore",
+  trend_model = "bs", n_knots = 7,
+  # varIndx = 1:nrow(simDat50$y_sim), # mapping of unique R matrix variances to indicators
+  iter = iter, chains = chains, thin = 1)
